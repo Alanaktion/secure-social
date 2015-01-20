92a0e491bc4a6d5a21818bf8a5c465e3ce5c20c8
Alan Hardman <alanaktion@gmail.com>
2015-01-19 22:01:41 -0700

    Minor cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,53 +11,8 @@ var App = {
 			return;
 		}
 		openpgp.initWorker(home + 'assets/js/openpgp.worker.min.js');
+		openpgp.config.show_version = false;
+		openpgp.config.show_comment = false;
 	},
 
-	//! Application development tests
-	test: {
-
-		//! Last generated PGP key
-		key: {},
-
-		/**
-		 * Generate a PGP key and show an alert with the result
-		 */
-		keygen: function() {
-			var options = {
-				numBits: 2048,
-				userId: 'Test User <test@example.org>',
-				passphrase: 'letmein'
-			};
-			openpgp.key.generate(options).then(function(data) {
-				App.test.key = data;
-				$('.alert-success').html('<p>Generated key with fingerprint: ' + data.primaryKey.fingerprint + '</p>')
-					.show()
-					.after('<pre>' + data.toPublic().armor() + '</pre>')
-					.after('<pre>' + data.armor() + '</pre>');
-				console.log(data);
-			});
-		},
-
-		/**
-		 * Encrypt a test message
-		 */
-		message: function() {
-			var str = "This is a test message.";
-			var publicKey = openpgp.key.readArmored(App.test.key.toPublic().armor());
-			openpgp.encryptMessage(publicKey.keys, str).then(function(data) {
-				console.log(data);
-			}, function(data) {
-				console.error("FAIL!");
-				console.error(data);
-			});
-		}
-
-	}
-
-};
-
-var User = {
-	id: "",
-	publicKey: "",
-	privateKey: "",
 };
